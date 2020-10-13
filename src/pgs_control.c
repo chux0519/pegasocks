9b4211d22a001a4c672c85660266b338956dd09a
hexyoungs <chuxdesign@hotmail.com>
2020-10-13 14:56:04 +0800

    feat: implement control commands

--- src/pgs_control.c
+++ src/pgs_control.c
@@ -1,10 +1,28 @@
 #include "pgs_control.h"
+#include <ctype.h>
+
+#define zero_to_minus_one(n) (((n) == 0) ? (-1) : (n))
+
+const char controller_herp_msg[] =
+	"Support commands: PING | GET SERVERS | SET SERVER $idx\n";
 
 static void accept_conn_cb(pgs_listener_t *listener, pgs_socket_t fd,
 			   pgs_sockaddr_t *address, int socklen, void *ctx);
 static void accept_error_cb(pgs_listener_t *listener, void *ctx);
 static void on_control_read(pgs_bev_t *bev, void *ctx);
 static void on_control_event(pgs_bev_t *bev, short events, void *ctx);
+
+static bool starts_with(const char *pre, const char *str)
+{
+	size_t lenpre = strlen(pre), lenstr = strlen(str);
+	if (lenstr < lenpre)
+		return false;
+	for (size_t i = 0; i < lenpre; i++) {
+		if (tolower(pre[i]) != tolower(str[i]))
+			return false;
+	}
+	return true;
+}
 
 void pgs_control_server_start(int fd, pgs_ev_base_t *base,
 			      pgs_server_manager_t *sm, pgs_logger_t *logger,
@@ -85,13 +103,51 @@ static void on_control_read(pgs_bev_t *bev, void *ctx)
 
 	pgs_size_t len = pgs_evbuffer_get_length(input);
 	unsigned char *rdata = pgs_evbuffer_pullup(input, len);
-	pgs_logger_debug_buffer(control_ctx->logger, rdata, len);
 
 	// Support commands are
-	// PING | GET SERVERS | GET METRICS | GET SERVER INDEX | SET SERVER $idx
+	// PING | GET SERVERS | SET SERVER $idx
+	if (starts_with("PING", (const char *)rdata)) {
+		pgs_evbuffer_add(output, "PONG\n", 5);
+	} else if (starts_with("GET SERVERS", (const char *)rdata)) {
+		pgs_server_config_t *servers = control_ctx->config->servers;
+		pgs_server_stats_t *stats = control_ctx->sm->server_stats;
+		int cur_server_index = control_ctx->sm->cur_server_index;
+		pgs_evbuffer_add_printf(output, "\n");
+		for (int i = 0; i < control_ctx->config->servers_count; i++) {
+			if (cur_server_index == i)
+				pgs_evbuffer_add_printf(output, "*");
+			else
+				pgs_evbuffer_add_printf(output, " ");
+			pgs_evbuffer_add_printf(output, "%d: %s\n", i,
+						servers[i].server_address);
+			pgs_evbuffer_add_printf(
+				output,
+				"\t%-8s| connect:%*.0f ms | g204:%*.0f ms\n",
+				servers[i].server_type, 6,
+				zero_to_minus_one(stats[i].connect_delay), 6,
+				zero_to_minus_one(stats[i].g204_delay));
+		}
+		pgs_evbuffer_add_printf(output, "\n");
+	} else if (starts_with("SET SERVER", (const char *)rdata)) {
+		int idx = atoi((const char *)&rdata[10]);
+		if (idx < control_ctx->config->servers_count) {
+			// Notice: no lock here
+			// do not change with systray and control port at the same time
+			pgs_logger_info(
+				control_ctx->logger,
+				"switched to server %s, index: %d",
+				control_ctx->config->servers[idx].server_address,
+				idx);
+			control_ctx->sm->cur_server_index = idx;
+			pgs_evbuffer_add_printf(output, "OK\n");
+		}
 
-	// shutdown this
-	pgs_bev_free(bev);
+	} else {
+		pgs_evbuffer_add(output, controller_herp_msg,
+				 strlen(controller_herp_msg));
+	}
+
+	pgs_evbuffer_drain(input, len);
 }
 
 static void on_control_event(pgs_bev_t *bev, short events, void *ctx)
