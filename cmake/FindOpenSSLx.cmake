984a6bb51b9e4790d2e7eff8569266f863c5b58c
hexyoungs <chuxdesign@hotmail.com>
2020-11-05 19:55:30 +0800

    cmake(osx): detect openssl
    instead of hardcoded one

--- cmake/FindOpenSSLx.cmake
+++ cmake/FindOpenSSLx.cmake
@@ -1,7 +1,13 @@
 if (APPLE)
     # This is a bug in CMake that causes it to prefer the system version over
     # the one in the specified ROOT folder.
-    set(OPENSSL_ROOT_DIR ${OPENSSL_ROOT_DIR} /usr/local/Cellar/openssl@1.1/1.1.1g/)
+    execute_process (
+        COMMAND sh -c "openssl version | cut -d ' ' -f2"
+        OUTPUT_VARIABLE OPENSSL_VERSION
+    )
+    string(REGEX REPLACE "\n$" "" OPENSSL_VERSION "${OPENSSL_VERSION}")
+    set(OPENSSL_ROOT_DIR ${OPENSSL_ROOT_DIR} /usr/local/Cellar/openssl@1.1/${OPENSSL_VERSION}/)
+    MESSAGE(STATUS "Found openssl library root: ${OPENSSL_ROOT_DIR}")
     set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_ROOT_DIR}/lib/libcrypto.dylib CACHE FILEPATH "" FORCE)
     set(OPENSSL_SSL_LIBRARY ${OPENSSL_ROOT_DIR}/lib/libssl.dylib CACHE FILEPATH "" FORCE)
     set (CMAKE_C_FLAGS -I${OPENSSL_INCLUDE_DIR})
