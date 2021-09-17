0525dd5a86d83e6ac751f5a5aacbd92481289ffa
Yongsheng Xu <chuxdesign@hotmail>
2021-09-17 15:20:25 +0800

    feat: add most of the acl checking logic

--- src/pgs_acl.c
+++ src/pgs_acl.c
@@ -1,31 +1,84 @@
 #include "pgs_acl.h"
 
-#include <ipset/ipset.h>
+#include <stdlib.h>
+#include <string.h>
 
-static struct ip_set white_list_ipv4;
-static struct ip_set white_list_ipv6;
-
-static struct ip_set black_list_ipv4;
-static struct ip_set black_list_ipv6;
-
-static int acl_mode = BLACK_LIST;
-
-int pgs_acl_init(const char *path)
+pgs_acl_t *pgs_acl_new(pgs_acl_mode mode, const char *path)
 {
-	ipset_init_library();
+	pgs_acl_t *ptr = malloc(sizeof(pgs_acl_t));
+	ptr->mode = mode;
+	ipset_init(&ptr->v4set);
+	ipset_init(&ptr->v6set);
+	cork_dllist_init(&ptr->rules);
 
-	ipset_init(&white_list_ipv4);
-	ipset_init(&white_list_ipv6);
-	ipset_init(&black_list_ipv4);
-	ipset_init(&black_list_ipv6);
+	// TODO: load acl file from path
 
-	return 0;
+	return ptr;
 }
 
-void pgs_acl_free()
+void pgs_acl_add_rule(pgs_acl_t *acl, const char *raw)
 {
-	ipset_done(&black_list_ipv4);
-	ipset_done(&black_list_ipv6);
-	ipset_done(&white_list_ipv4);
-	ipset_done(&white_list_ipv6);
+	pgs_acl_rule_t *rule = pgs_acl_rule_new(raw);
+	cork_dllist_add(&acl->rules, &rule->entry);
+}
+
+void pgs_acl_free(pgs_acl_t *ptr)
+{
+	ipset_done(&ptr->v4set);
+	ipset_done(&ptr->v6set);
+
+	struct cork_dllist_item *iter;
+	while ((iter = cork_dllist_head(&ptr->rules)) != NULL) {
+		pgs_acl_rule_t *rule =
+			cork_container_of(iter, pgs_acl_rule_t, entry);
+		cork_dllist_remove(&rule->entry);
+		pgs_acl_rule_free(rule);
+	}
+	free(ptr);
+	ptr = NULL;
+}
+
+pgs_acl_rule_t *pgs_acl_rule_new(const char *raw)
+{
+	pgs_acl_rule_t *ptr = malloc(sizeof(pgs_acl_rule_t));
+	ptr->raw = strdup(raw);
+	ptr->pattern = re_compile(ptr->raw);
+	return ptr;
+}
+
+void pgs_acl_rule_free(pgs_acl_rule_t *ptr)
+{
+	free(ptr->raw);
+	free(ptr);
+	ptr = NULL;
+}
+
+bool pgs_acl_match_host(pgs_acl_t *acl, const char *host)
+{
+	struct cork_ip addr;
+	int err = cork_ip_init(&addr, host);
+
+	if (err) {
+		int match_length;
+		int match_idx = -1;
+		struct cork_dllist_item *curr, *next;
+		pgs_acl_rule_t *rule;
+		cork_dllist_foreach(&acl->rules, curr, next, pgs_acl_rule_t,
+				    rule, entry)
+		{
+			match_idx =
+				re_matchp(rule->pattern, host, &match_length);
+			if (match_idx != -1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	if (addr.version == 4) {
+		return ipset_contains_ipv4(&acl->v4set, &(addr.ip.v4));
+	} else if (addr.version == 6) {
+		return ipset_contains_ipv6(&acl->v6set, &(addr.ip.v6));
+	}
+	return false;
 }
--- src/pgs_acl.h
+++ src/pgs_acl.h
@@ -1,16 +1,40 @@
 #ifndef _PGS_ACL
 #define _PGS_ACL
 
-#define BLACK_LIST 0
-#define WHITE_LIST 1
+#include <stdbool.h>
 
-int pgs_acl_init(const char *path);
-void pgs_acl_free();
+#include <ipset/ipset.h>
+#include <libcork/ds/dllist.h>
+#include <tiny-regex-c/re.h>
 
-int pgs_acl_match_host(const char *ip);
-int pgs_acl_add_ip(const char *ip);
-int pgs_acl_remove_ip(const char *ip);
+typedef enum {
+	PROXY_ALL_BYPASS_LIST,
+	BYPASS_ALL_PROXY_LIST,
+} pgs_acl_mode;
 
-int pgs_get_acl_mode();
+typedef struct pgs_acl_s {
+	pgs_acl_mode mode;
+	struct ip_set v4set;
+	struct ip_set v6set;
+	struct cork_dllist rules;
+} pgs_acl_t;
+
+typedef struct pgs_acl_rule_s {
+	char *raw;
+	re_t pattern;
+	struct cork_dllist_item entry;
+} pgs_acl_rule_t;
+
+pgs_acl_t *pgs_acl_new(pgs_acl_mode mode, const char *path);
+
+void pgs_acl_add_rule(pgs_acl_t *acl, const char *raw);
+
+void pgs_acl_free(pgs_acl_t *acl);
+
+pgs_acl_rule_t *pgs_acl_rule_new(const char *raw);
+
+void pgs_acl_rule_free(pgs_acl_rule_t *rule);
+
+bool pgs_acl_match_host(pgs_acl_t *acl, const char *host);
 
 #endif
